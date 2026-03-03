ab283c5245807565915fb053a04c6b1d953381d1
baao09 <baothai80004@gmail.com>
2026-03-03 19:20:29 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
 dotenv.config();
 
 const app = express();
+app.use(cors());
 app.use(express.json({ limit: "1mb" }));
 // Đảm bảo bạn đã cho các file giao diện vào thư mục public như tôi đã nhắc ở trên
 app.use(express.static("public"));
